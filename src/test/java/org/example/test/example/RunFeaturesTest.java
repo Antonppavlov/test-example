19ad6ef2d80f93fd2ed148109a3dd3da9f10dd57
Antonpavlov <anton.it.pavlov@gmail.com>
2020-10-21 17:39:56 +0300

    Initial commit

---
+++ src/test/java/org/example/test/example/RunFeaturesTest.java
@@ -0,0 +1,18 @@
+package org.example.test.example;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        monochrome = true,
+        plugin = {"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm", "pretty"},
+        tags = "@unit",
+        features = "src/test/resources/features",
+        glue = {"ru"}
+)
+public class RunFeaturesTest {
+
+}
